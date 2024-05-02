63f07b9269d3ce0795cdea00cf80b9d067252849
Jean-Clément <jeanclement@audusseau.fr>
2024-05-02 16:11:09 +0200

    create python password manager project

---
+++ gestion_mdp.py
@@ -0,0 +1,25 @@
+# import hashlib
+# import secrets
+
+# pinceDeSel = secrets.token_hex(64)
+
+# mdp = input("qu’est-ce que c’est ton mot de passe ? : ")
+
+# algoChiffre = hashlib.sha3_512((mdp + pinceDeSel).encode())
+# hash = algoChiffre.hexdigest()
+# print(hash)
+# fichierTopSecret = open("mesMotsDePasse", "a")
+# fichierTopSecret.write(hash + "\n")
+# fichierTopSecret.close()
+
+import bcrypt
+
+mdp = input("qu’est-ce que c’est ton mot de passe ? : ").encode()
+
+pinceDeSel = bcrypt.gensalt()
+algoChiffre = bcrypt.hashpw(mdp, pinceDeSel)
+print(algoChiffre)
+
+fichierTopSecret = open("mesMotsDePasse", "a")
+fichierTopSecret.write(algoChiffre.decode() + "\n")
+fichierTopSecret.close()
